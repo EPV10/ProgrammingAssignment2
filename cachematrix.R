7a7de3527c0a1b65522416a1e1f56d97c886a0c3
EPV10 <eunice.pondigo@gmail.com>
2016-03-05 01:01:28 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a special "matrix", which is a list containing
+## a function to
+##   - set the value of the matrix
+##   - get the value of the matrix
+##   - set the value of the inverse matrix
+##   - get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cache <- NULL                               # clear cache 
+    
+    set <- function(new_matrix) {               # Strore a matrix     
+        x <<- new_matrix
+        cache <<- NULL                          # since the matrix is assigned a new value, clear the cache
+    }
+    
+    get <- function(){
+        x                                       # return the stored matrix
+    }
+    
+    setinverse <- function(inv) {               # Cache the given argument
+        cache <<- inv
+    }
+    
+    getinverse <- function() {                  # Return the cache value
+         cache
+    }
+    
+    list(
+        set = set,
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse
+    )
 }
 
 
-## Write a short comment describing this function
+## Calculate the inverse of the special "matrix" created with the above
+## function, reusing cached result if it is available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cache <- x$getinverse()                         # get the cached value
+    if(!is.null(cache)) {                           # if a cached value exists return it
+        message("getting cached data")
+        return(i)
+    }
+    m <- x$get()                                   # otherwise get the matrix, caclulate the inverse and store it in the cache
+    cache <- solve(m, ...)
+    x$setinverse(cache)
+    cache
 }
+
